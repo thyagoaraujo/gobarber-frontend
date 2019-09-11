b4a6c8c1377f59ac6c6db1a2d9cd65b45e7c7db4
Thyago Araujo <thyagopcb.araujo@gmail.com>
2019-09-11 16:31:26 -0300

    add notifications

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,7 +16,7 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  plugins: ['react', 'prettier', 'react-hooks', 'import', 'jsx-ally'],
+  plugins: ['react', 'prettier', 'react-hooks', 'import', 'jsx-a11y'],
   rules: {
     'prettier/prettier': 'error',
     'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
